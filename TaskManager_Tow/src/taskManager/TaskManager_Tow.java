0afc97e50062e1d5a0c6af88368597a9c4c16537
Insik <Insik@172.16.165.156>
2021-09-16 13:30:46 +0900

    fix data source jms broker address

--- TaskManager_Tow/src/taskManager/TaskManager_Tow.java
+++ TaskManager_Tow/src/taskManager/TaskManager_Tow.java
@@ -141,7 +141,7 @@ public class TaskManager_Tow extends ArbiAgent {
 	public void onStart() {
 		dc = new TaskManagerDataSource(this);
 
-		dc.connect(JMS_BROKER_URL, DATASOURCE_PREFIX + TASKMANAGER_ADRESS,2);
+		dc.connect("tcp://" + ENV_JMS_BROKER, DATASOURCE_PREFIX + TASKMANAGER_ADRESS,2);
 
 		System.out.println("======Start Test Agent======");
 		System.out.println("??");
